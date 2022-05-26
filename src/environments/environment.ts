11a0b8e36c26361b392fd947ff49f91fd2b43a46
peterc68 <106084277+peterc68@users.noreply.github.com>
2022-05-26 14:31:29 +0200

    Added a comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// add a more comments to create a new push
 export const environment = {
   production: false,
   apiUrl: "http://localhost:8080/api/v1/lolchampion/"
