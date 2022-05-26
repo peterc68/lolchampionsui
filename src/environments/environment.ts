a1ad5a5579fbbbe9a7582290203f295e56a61fb6
peterc68 <106084277+peterc68@users.noreply.github.com>
2022-05-26 15:34:54 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// new commit
 export const environment = {
   production: false,
   apiUrl: "https://lol-champions-ose2-champs.apps.ose.openshift.conygre.com/api/v1/lolchampion/"
